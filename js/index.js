f77f461fe8937ae3db4863daa6b7b3579525b977
JuanSantillan97 <juanyut97@hotmail.com>
2019-01-30 14:58:46 -0600

    seccion websites y footer

--- js/index.js
+++ js/index.js
@@ -2,5 +2,17 @@
 ((d) => {
   d.addEventListener('DOMContentLoaded', e => {
     M.AutoInit()
+
+    const sliders = d.querySelectorAll('.slider')
+
+    M.Slider.init(sliders, {
+      indicators: true,
+      duration: 1000
+    })
+
+    const copy = d.querySelector('.Footer small')
+
+    copy.innerHTML = `&copy; ${new Date().getFullYear()} @juan_yut`
+
   })
 }) (document);
