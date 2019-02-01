8a983e832577aaa368427707c5ede6cb0e615e9f
JuanSantillan97 <juanyut97@hotmail.com>
2019-02-01 17:32:13 -0600

    final proyecto pt1

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 **Juan Santillán**
 
 Pagina web con algo de materializecss lml
+
+<p align="center">
+  <img src="img/myMemeWeb.png" width="1888" height="12495"/>
+</p>
--- js/index.js
+++ js/index.js
@@ -10,6 +10,21 @@
       duration: 1000
     })
 
+    // Hacer full screen el carousel.
+
+    const carousels = d.querySelectorAll('.carousel')
+    M.Carousel.init(carousels, {
+      fullWidth: true
+    })
+
+    // Mover automaticamente el carousel.
+
+    const instantCarousel = M.Carousel.getInstance(d.querySelector('.carousel'))
+
+    setInterval(() => {
+      instantCarousel.next(1)
+    }, 5000)
+
     const copy = d.querySelector('.Footer small')
 
     copy.innerHTML = `&copy; ${new Date().getFullYear()} @juan_yut`
